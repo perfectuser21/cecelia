83801a32d0aeb6baced2b315f341cd85bd97f1e0
Alex <alexperfectapi01@gmail.com>
2026-05-17 08:40:16 +0800

    test(deploy): brain-deploy-cleanup-test.sh 验证精确容器名清理

---
+++ scripts/brain-deploy-cleanup-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+node -e "
+const fs = require('fs');
+const src = fs.readFileSync('scripts/brain-deploy.sh', 'utf8');
+if (src.includes('--filter \"status=exited\"')) {
+  console.log('FAIL: 仍有 status=exited filter');
+  process.exit(1);
+}
+if (!src.includes('^/cecelia-node-brain\$')) {
+  console.log('FAIL: 未找到精确 name filter');
+  process.exit(1);
+}
+console.log('PASS: brain-deploy.sh 使用精确容器名清理');
+"
